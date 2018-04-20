a00db0669ea0b28fa08d26389775061e9f75e3ac
tiantian <wangtiantian139@gmail.com>
2018-04-20 06:14:06 +0000

    rewrite the data structure. some error: infinite loop.

---
+++ document_process.py
@@ -0,0 +1,361 @@
+"""
+        Document Process
+
+This file is to define a class where is
+storing the processed data from original documents.
+
+The necessary components are
+ (a) Original sentences which classified by documents,
+ (b) Stop-word-free and stemmed sentences which signed
+  with index ordered by where the original sentences is,
+ (c) A dictionary where the unique words appeared in all
+  of the documents which are regarded as keys, which
+  indicated by the key is a numpy-array-based list.
+  For one of that lists, the indexes relate to document's
+  ones as well as elements are arrays of the counts of
+  word ordered by sentence indexes.
+
+However, it is easy for using to define the components
+mentioned above as public ones.
+
+Plus, some methods for using the private data are in need,
+such as abstract the interested text from a document list,
+which can defined as a protected method.
+
+data structure
+
+ doc : n(doc) x m(sen) (sen = {str})
+   doc0[ [sen1, sen2, sen3, ... ,senk],
+   doc1| [sen(k+1), sen(k+2), ... ]
+   ... | [ ... ]
+   docn] [ ... , senm]
+
+ word_counter : q(wrd)
+       { 'wrd0' : [ value_list0 ]
+       | 'wrd1' : [ value_list1 ]
+       |   ...  :      ...
+       } 'wrdq' : [ value_listq ]
+
+ value_list - for word_counter : n(doc) x m(sen) (sen = {int})
+   doc0[ [sen1, sen2, sen3, ... ,senk],
+   doc1| [sen(k+1), sen(k+2), ... ]
+   ... | [ ... ]
+   docn] [ ... , senm]
+
+"""
+
+from poter_stemming import PorterStemmer
+
+REFERENCE_PATH = 'doc/reference/'
+STOP_WORD_LIST = 'stop-word-list.csv'
+
+'''
+Split text in the document into sentences.return a list, which contains sub-lists 
+of sentences in a document, of all the text of the documents in the DOCUMENTS tuple.  
+NOTE that there are some sentences are not to be recognized such as ones end with '?' 
+or end with '.\'\'', as well as some extra punctuation such as `` or '' are remained
+too.  
+'''
+
+
+def split_sentences(path: str, documents: tuple):
+    doc = []
+    # transverse every document
+    for f in documents:
+        # open the current document
+        infile = open(path + f, 'r')
+        str_text = ''
+        # skip line until the text start signal has been read
+        while infile.readline() != '<TEXT>\n':
+            continue
+        # store all text sentences without '\n' into sentences
+        while True:
+            line = infile.readline()
+            if line != '</TEXT>\n':
+                str_text += line[:-1]  # the last char is always '\n'
+            else:
+                break
+        sentences = str_text.split('. ')
+        # if the last sentence is empty string, abandon it
+        if sentences[-1] == '':
+            sentences = sentences[:-1]
+        doc.append(sentences)
+        infile.close()
+    return doc
+
+
+'''
+delete stop word in sentence as well as transform the sentence into 
+lower case 
+'''
+
+
+def delete_stop_words(ori_sen: str):
+    # for identifying the real stop word
+    # from sub-string in a word which is as same
+    # as some stop word, it is more simple to add
+    # space in the front and at the end of the sentence,
+    # while using ' ' + word + ' ' to identify the stop
+    # word
+    sentence = ' ' + ori_sen.lower() + ' '
+    # read stop word list in file
+    infile = open(REFERENCE_PATH + STOP_WORD_LIST)
+    # read and transform into lower case
+    lines = infile.read().lower()
+    stop_word_list = lines.split(', ')
+    # splitting into words is to avoid form deleting some
+    # characters in a certain word which has a sub-string
+    # containing the stop word
+    words = sentence.split(' ')
+    for w in words:
+        if w in stop_word_list:
+            sentence = sentence.replace(' ' + w + ' ', ' ')
+        else:
+            continue
+    # remove the spaces in the beginning and at the end,
+    # which were added in the beginning of this method
+    return sentence[1:-1]
+
+
+'''
+use class PorterStemmer written by Porter
+return a list of stemmed sentences 
+'''
+
+
+def porter_stemming(sentences: list):
+    p = PorterStemmer()
+    new_sentences = []
+    for s in sentences:
+        output = ''
+        word = ''
+        for c in s:
+            if c.isalpha():
+                word += c.lower()
+            else:
+                if word:
+                    output += p.stem(word, 0, len(word) - 1)
+                    word = ''
+                output += c.lower()
+        new_sentences.append(output)
+    return new_sentences
+
+
+def count_word(sentence: str):
+    w = ''
+    counter_in_sen = {}
+    for c in sentence:
+        if c.isalpha():
+            w += c.lower()
+        else:
+            if w.__len__() <= 1:
+                continue
+            if w not in counter_in_sen.keys():
+                counter_in_sen[w] = 1
+                w = ''
+            else:
+                counter_in_sen[w] += 1
+                w = ''
+    return counter_in_sen
+
+
+class WordCounter:
+    def __init__(self):
+        self.cntr = {}
+
+    def generate_value_list(self, doc: list, word: str):
+        ori_cnt_lst = []
+        for d in doc:
+            counter_in_sen = [0 for i in range(d.__len__())]
+            ori_cnt_lst.append(counter_in_sen)
+        self.cntr[word] = ori_cnt_lst
+
+
+class DocProcess:
+
+    def __init__(self, doc_path: str, doc_list: tuple):
+        # a list to store all sentences in every document
+        self.ori_doc = []
+
+        # a list of Stop-word-free and stemmed sentences which signed
+        # with index ordered by where the original sentences is,
+        self.processed_doc = []
+
+        # a list of unique words
+        self.word_list = []
+
+        # a dictionary of the count of appeared words
+        self.word_counter = WordCounter()
+
+        # split text in doc into sentences
+        self.ori_doc = split_sentences(path=doc_path, documents=doc_list)
+        # delete stop words and stem remained words
+        # store the processed data in processed_doc
+        for old_doc in self.ori_doc:
+            new_doc = []
+            for s in old_doc:
+                new_doc.append(delete_stop_words(s))
+            new_doc = porter_stemming(new_doc)
+            self.processed_doc.append(new_doc)
+
+        # count words
+        doc_idx = 0
+        for pro_doc in self.processed_doc:
+            sen_idx = 0
+            for sen in pro_doc:
+                counter_in_sen = count_word(sen)
+                for w in counter_in_sen.keys():
+                    if w in self.word_counter.cntr.keys():
+                        self.word_counter.cntr[w][doc_idx][sen_idx] += 1
+                    else:
+                        # if not add the word, init the value 2-level list with 0
+                        self.word_counter.generate_value_list(self.processed_doc, w)
+                        self.word_counter.cntr[w][doc_idx][sen_idx] += 1
+                sen_idx += 1
+            doc_idx += 1
+        self.word_list = self.word_counter.cntr.keys()
+
+    def count_in_sen(self, word, doc_index, sen_index):
+        if word in self.word_counter.cntr.keys():
+            return self.word_counter.cntr[word][doc_index][sen_index]
+        else:
+            return -1
+
+    def count_in_doc(self, word, doc_index):
+        if word in self.word_counter.cntr.keys():
+            sum_count = 0
+            for sen_idx in range(self.processed_doc[doc_index].__len__()):
+                sum_count += self.word_counter.cntr[word][doc_index][sen_idx]
+            return sum_count
+        else:
+            return -1
+
+    def count_total(self, word):
+        if word in self.word_counter.cntr.keys():
+            counter = 0
+            for doc_idx in range(self.processed_doc.__len__()):
+                counter += self.count_in_doc(word, doc_idx)
+            return counter
+        else:
+            return -1
+
+    # count how many docs containing the word
+    def count_doc(self, word):
+        if word in self.word_counter.cntr.keys():
+            counter = 0
+            for doc_idx in range(self.doc_size()):
+                for sen_idx in range(self.sen_size(doc_idx)):
+                    if self.count_in_sen(word, doc_idx, sen_idx):
+                        counter += 1
+                        break
+                    else:
+                        continue
+            return counter
+        else:
+            return -1
+
+    def doc_size(self):
+        return self.processed_doc.__len__()
+
+    def sen_size(self, doc_index):
+        return self.processed_doc[doc_index].__len__()
+
+    def sen_size_total(self):
+        sum = 0
+        for doc_idx in range(self.doc_size()):
+            sum += self.sen_size(doc_idx)
+        return sum
+
+    def word_size(self, doc_index):
+        sum = 0
+        for sen in self.processed_doc[doc_index]:
+            counter_in_sen = count_word(sen)
+            for v in counter_in_sen.values():
+                sum += v
+        return sum
+
+    def word_size_total(self):
+        sum = 0
+        for doc_index in range(self.doc_size()):
+            for sen in self.processed_doc[doc_index]:
+                counter_in_sen = count_word(sen)
+                for v in counter_in_sen.values():
+                    sum += v
+        return sum
+
+    def abstract(self, sen_rank):
+        for doc in self.ori_doc:
+            if sen_rank >= doc.__len__():
+                sen_rank -= doc.__len__()
+            else:
+                return doc[sen_rank]
+
+
+def basic_test():
+    SPLIT_CACHE = 'doc/cache/split.cache'
+    DEL_STOP_WORD_CACHE = 'doc/cache/del_stop_word.cache'
+    STEM_CACHE = 'doc/cache/stem.cache'
+    # test split sentences
+    outfile = open(SPLIT_CACHE, 'w')
+    doc_index = 0
+    # split the text in docs into sentences
+    doc_sentences = split_sentences(DOC_PATH, DOCUMENTS)
+    for sentences in doc_sentences:
+        # output <index> to indicate the beginning of the current document
+        outfile.write('<' + str(doc_index) + '>\n')
+        for s in sentences:
+            outfile.write(s + '\n')
+        # output <\index> to indicate the end of the current document
+        outfile.write('</' + str(doc_index) + '>\n')
+        outfile.write('\n')
+        doc_index += 1
+    print('The unprocessed data has been split into sentences by space. ')
+    print('The result is output in ' + SPLIT_CACHE)
+    outfile.close()
+    # test deleting stop word
+    infile = open(SPLIT_CACHE, 'r')
+    outfile = open(DEL_STOP_WORD_CACHE, 'w')
+    lines = infile.read()
+    lines = lines.split('\n')
+    # delete stop words
+    for l in lines:
+        sentence = delete_stop_words(l)
+        outfile.write(sentence + '\n')
+    print('The split sentences has been free of stop words. ')
+    print('The result is output in ' + DEL_STOP_WORD_CACHE)
+    outfile.close()
+    # test stemming
+    infile = open(DEL_STOP_WORD_CACHE, 'r')
+    outfile = open(STEM_CACHE, 'w')
+    lines = infile.read()
+    lines = lines.split('\n')
+    # stem sentences of all lines
+    sentences = porter_stemming(lines)
+    for s in sentences:
+        outfile.write(s + '\n')
+    print('The stop-word-free sentences has been stemmed. ')
+    print('The result is output in ' + STEM_CACHE)
+    outfile.close()
+
+
+'''
+// main //
+'''
+
+if __name__ == "__main__":
+    DOC_PATH = 'doc/unprocessed_data/d30045t/'
+    DOCUMENTS = ('NYT19981125.0417',
+                 'NYT19981125.0433',
+                 'NYT19981126.0192',
+                 'NYT19981127.0203',
+                 'NYT19981127.0240',
+                 'NYT19981127.0256',
+                 'NYT19981127.0264',
+                 'NYT19981127.0289',
+                 'NYT19981127.0293',
+                 'NYT19981129.0113',
+                 )
+    doc = DocProcess(DOC_PATH, DOCUMENTS)
+    doc6 = doc.ori_doc[6]
+    p_doc6 = doc.processed_doc[6]
+    pass
--- document_summarization.py
+++ document_summarization.py
@@ -1,6 +1,33 @@
-import math
+"""
+TF-IDF algorithm
 
-from poter_stemming import PorterStemmer
+input: a list which includes
+       stemmed and stop-word-free sentence sub-lists
+       obtained from original documents
+output: a list whose index indicates the sentences in
+        original document and whose value is a sub-list
+        of the TF-IDF values, where each value combining
+        a unique word appeared in the doc in which the
+        sentence comes.
+
+the target of this algorithm is to obtain a matrix
+where rows as sentences while columns as words.
+
+formula:
+ | for a word in the word list, whose:
+ | TF_IDF_value = TF * IDF;
+ | (scope: one doc)
+ | TF_value = count_of_the_word / count_of_all_words;
+ | (scope: all docs)
+ | IDF_value = log(count_of_the_docs / (count_of_the_docs_containing_word + 1);
+
+"""
+
+
+import math
+import numpy as np
+from document_process import DocProcess
+
 
 DOC_PATH = 'doc/unprocessed_data/d30045t/'
 REFERENCE_PATH = 'doc/reference/'
@@ -16,182 +43,7 @@ DOCUMENTS = ('NYT19981125.0417',
              'NYT19981129.0113',
              )
 STOP_WORD_LIST = 'stop-word-list.csv'
-SPLIT_CACHE = 'doc/cache/split.cache'
-DEL_STOP_WORD_CACHE = 'doc/cache/del_stop_word.cache'
-STEM_CACHE = 'doc/cache/stem.cache'
 
-'''
-def split_sentences():
-    outfile = open(PROCESS_CACHE, 'w')
-    doc_index = 0
-    for f in DOCUMENTS:
-        infile = open(DOC_PATH + f, 'r')
-        
-        # output <index> to indict the beginning of the current document
-        outfile.write('<' + str(doc_index) + '>\n')
-        sentence = ''
-        # if there is '.' the flag are to be set
-        # the flag makes sentence is not split until
-        # the next letter is big case, which is supposed
-        # to be the signal of a new sentence
-        sentence_flag = False
-        
-        while infile.readline() != '<TEXT>\n':
-            continue
-        while True:
-            line = infile.readline()
-            # if not the end of the text
-            if line != '</TEXT>\n':
-                # check every character in line
-                for char in line:
-                    if char == '\n':
-                        continue
-                    # ignore '``'
-                    if char == '`':
-                        continue
-                    # ignore '\'\'' but the shortcoming is
-                    # the '\'' such as 'it's' will become 'its'
-                    if char == '\'':
-                        continue
-                    elif char == '.':
-                        if sentence_flag:
-                            # delete the space which appears
-                            # in the front of the sentence
-                            if sentence[0] == ' ':
-                                sentence = sentence[1:]
-                            outfile.write(sentence + '\n')
-                            sentence = ''
-                        else:  # sentence_flag unset
-                            sentence_flag = True
-                    else:
-                        sentence += char
-            # if the end of the text, switch to the next document
-            else:  # line == '</TEXT>\n'
-                # output <\index> to indict the end of the current document
-                outfile.write('</' + str(doc_index) + '>\n')
-                doc_index += 1
-                break
-    outfile.close()
-'''
-
-'''
-TF-IDF algorithm 
-
-input: a list which includes 
-       stemmed and stop-word-free sentence sub-lists
-       obtained from original documents
-output: a list whose index indicates the sentences in 
-        original document and whose value is a sub-list 
-        of the TF-IDF values, where each value combining
-        a unique word appeared in the doc in which the
-        sentence comes. 
-
-the target of this algorithm is to obtain a matrix
-where rows as sentences while columns as words.
-
-formula:
- | for a word in the word list, whose:
- | TF_IDF_value = TF * IDF;
- | (scope: one doc)
- | TF_value = count_of_the_word / count_of_all_words;
- | (scope: all docs)
- | IDF_value = log(count_of_the_docs / (count_of_the_docs_containing_word + 1); 
-
-'''
-
-'''
-Split text in the document into sentences.return a list, which contains sub-lists 
-of sentences in a document, of all the text of the documents in the DOCUMENTS tuple.  
-NOTE that there are some sentences are not to be recognized such as ones end with '?' 
-or end with '.\'\'', as well as some extra punctuation such as `` or '' are remained
-too.  
-'''
-
-
-def split_sentences(doc_path=DOC_PATH, documents=DOCUMENTS):
-    doc = []  # a list to store all sentences in every document
-    # transverse every document
-    for f in documents:
-        # open the current document
-        infile = open(doc_path + f, 'r')
-        str_text = ''
-        # skip line until the text start signal has been read
-        while infile.readline() != '<TEXT>\n':
-            continue
-        # store all text sentences without '\n' into sentences
-        while True:
-            line = infile.readline()
-            if line != '</TEXT>\n':
-                str_text += line[:-1]  # the last char is always '\n'
-            else:
-                break
-        sentences = str_text.split('. ')
-        # if the last sentence is empty string, abandon it
-        if sentences[-1] == '':
-            sentences = sentences[:-1]
-        doc.append(sentences)
-        infile.close()
-    # / for f in DOCUMENTS
-    # since all sentences which are saved as sub-lists in doc_sentences
-    # has been simply split, now the doc_sentences
-    return doc
-
-
-'''
-delete stop word in sentence as well as transform the sentence into 
-lower case 
-'''
-
-
-def delete_stop_words(sentence: str):
-    # for identifying the real stop word
-    # from sub-string in a word which is as same
-    # as some stop word, it is more simple to add
-    # space in the front and at the end of the sentence,
-    # while using ' ' + word + ' ' to identify the stop
-    # word
-    sentence = ' ' + sentence.lower() + ' '
-    # read stop word list in file
-    infile = open(REFERENCE_PATH + STOP_WORD_LIST)
-    # read and transform into lower case
-    lines = infile.read().lower()
-    stop_word_list = lines.split(', ')
-    # splitting into words is to avoid form deleting some
-    # characters in a certain word which has a sub-string
-    # containing the stop word
-    words = sentence.split(' ')
-    for w in words:
-        if w in stop_word_list:
-            sentence = sentence.replace(' ' + w + ' ', ' ')
-        else:
-            continue
-    # remove the spaces in the beginning and at the end,
-    # which were added in the beginning of this method
-    return sentence[1:-1]
-
-
-'''
-use class PorterStemmer written by Porter
-return a list of stemmed sentences 
-'''
-
-
-def porter_stemming(sentences: list):
-    p = PorterStemmer()
-    new_sentences = []
-    for s in sentences:
-        output = ''
-        word = ''
-        for c in s:
-            if c.isalpha():
-                word += c.lower()
-            else:
-                if word:
-                    output += p.stem(word, 0, len(word) - 1)
-                    word = ''
-                output += c.lower()
-        new_sentences.append(output)
-    return new_sentences
 
 
 '''
@@ -208,20 +60,6 @@ def tf(sentences: list):
     # in dictionary count_words only if the length of the word is
     # bigger than 1 as well as it is unique in words
     count_word = {}
-    for s in sentences:
-        w = ''
-        for c in s:
-            if c.isalpha():
-                w += c.lower()
-            else:
-                if w.__len__() <= 1:
-                    continue
-                if w not in count_word.keys():
-                    count_word[w] = 1
-                    w = ''
-                else:
-                    count_word[w] += 1
-                    w = ''
     tf_word = {}
     sum_count_words = 0
     for count_w in count_word.keys():
@@ -232,70 +70,50 @@ def tf(sentences: list):
 
 
 '''
-input is a document list, a list of word-count dictionary 
-in all docs and a list of unique words in all docs,
-output a idf list 
-'''
-
-
-def idf(document_list: list, doc_count_word: list, all_word: list):
-    count_docs = document_list.__len__()
-    idf_word = {}
-    for w in all_word:
-        count_word_in_doc = 0
-        # transverse the count dictionary in each doc
-        for count in doc_count_word:
-            if w in count.keys() and count[w] > 0:
-                count_word_in_doc += 1
-            else:
-                continue
-        idf_word[w] = math.log(count_docs / (count_word_in_doc + 1), 10)
-    return idf_word
-
-
-'''
 the main method of the TF-IDF algorithm
 '''
 
 
-def tf_idf(doc_path=DOC_PATH, documents=DOCUMENTS):
-    ori_doc = split_sentences(doc_path, documents)
-    new_doc = []
-    new_sentences = []
-    for sentences in ori_doc:
-        for s in sentences:
-            new_sentences.append(delete_stop_words(s))
-        new_sentences = porter_stemming(new_sentences)
-        new_doc.append(new_sentences)
-    doc_count_word = []
-    doc_tf_word = []
-    tfidf_matrix = []
-    all_word = []
-    for sentences in new_doc:
-        tf_word, count_word = tf(sentences)
-        doc_tf_word.append(tf_word)
-        doc_count_word.append(count_word)
-    for count_word in doc_count_word:
-        for word in count_word.keys():
-            if word not in all_word:
-                all_word.append(word)
-            else:
-                continue
-    idf_word = idf(document_list=documents,
-                   doc_count_word=doc_count_word,
-                   all_word=all_word)
-    for doc in new_doc:
-        i_doc = 0
-        for s in doc:
-            tfidf_sentence = []
-            for w in all_word:
-                if w in doc_tf_word[i_doc].keys() and s.find(w):
-                    tfidf_sentence.append(doc_tf_word[i_doc][w] * idf_word[w])
+def tf_idf(data: DocProcess):
+    # a sentence-word (row as sentence) matrix, whose element is TF-IDF value
+    s_w_matrix = np.zeros((data.sen_size_total(), data.word_size_total()))
+
+    # transverse every sentence
+    # the sentence index in multi-doc scope
+    general_sen_idx = 0
+    for doc_idx in range(data.doc_size()):
+        # the number of all of words in a certain doc
+        wrd_sz_total = data.word_size(doc_idx)
+        for sen_idx in range(data.sen_size(doc_idx)):
+            # the word index related to word list in data
+            wrd_idx = 0
+            for word in data.word_list:
+                # the number of how many times the word appears
+                # in a certain doc
+                count = data.count_in_doc(word, doc_idx)
+                # many of 0 cases, skip for the matrix has been initialized with 0
+                if count == 0:
+                    continue
+                elif count > 0:
+                    tf = count / wrd_sz_total
+                    idf = math.log(data.doc_size() / (data.count_doc(word) + 1), 10)
+                    s_w_matrix[general_sen_idx, wrd_idx] = tf * idf
+                elif count == -1:
+                    print('error. no such word', word)
                 else:
-                    tfidf_sentence.append(0)
-            tfidf_matrix.append(tfidf_sentence)
-        i_doc += 1
-    return tfidf_matrix
+                    print('error. counter error.')
+                wrd_idx += 1
+            general_sen_idx += 1
+    # regard all sentences in docs as a long one,
+    # and compute its TF-IDF value
+    long_sen_vector = []
+    word_size_total = data.word_size_total()
+    for word in data.word_list:
+        tf = data.count_total(word) / word_size_total
+        idf = math.log(1 / (1 + 1))
+        long_sen_vector.append(tf * idf)
+
+    return s_w_matrix, np.array(long_sen_vector)
 
 
 '''
@@ -303,8 +121,41 @@ cosine similarity algorithm
 '''
 
 
-def cos_similarity(sentence1: list, sentence2):
+def cos_similarity(sentence1: np.ndarray, sentence2: np.ndarray):
+    sen1_pro_sen2 = np.transpose(sentence1) * sentence2
+    amp_sen1, amp_sen2 = np.fabs(sentence1), np.fabs(sentence2)
+    return np.cos(sen1_pro_sen2 / (amp_sen1 * amp_sen2))
 
+
+def summarize(doc_path: str, doc_list: tuple):
+    data = DocProcess(doc_path, doc_list)
+    s_w_matrix, long_sen_vector = tf_idf(data)
+    # a list of cos similarity, whose element is a tuple of
+    # cosine value and original sentence's rank in sentence-word
+    # matrix
+    cos_sim_lst = []
+    for r in range(np.size(s_w_matrix, 0)):
+        cosine = cos_similarity(s_w_matrix[r], long_sen_vector)
+        cos_sim_lst.append((cosine, r))
+    cos_sim_lst.sort()
+    check_rank = 0
+    current_rank = 1
+    summary = [data.abstract(cos_sim_lst[check_rank][1])]
+    while current_rank != np.size(s_w_matrix, 0):
+        if summary.__sizeof__() >= 665:
+            break
+        else:
+            pass
+        # compare 2 sentences
+        cmp = cos_similarity(s_w_matrix[cos_sim_lst[current_rank][1]]
+                             , s_w_matrix[cos_sim_lst[check_rank][1]])
+        if cmp < 0.3:
+            summary.append(data.abstract(cos_sim_lst[current_rank][1]))
+            check_rank = current_rank
+            current_rank = check_rank + 1
+        else:
+            current_rank += 1
+    return summary
 
 
 '''
@@ -312,46 +163,6 @@ def cos_similarity(sentence1: list, sentence2):
 '''
 
 if __name__ == '__main__':
-    # test split sentences
-    outfile = open(SPLIT_CACHE, 'w')
-    doc_index = 0
-    # split the text in docs into sentences
-    doc_sentences = split_sentences()
-    for sentences in doc_sentences:
-        # output <index> to indicate the beginning of the current document
-        outfile.write('<' + str(doc_index) + '>\n')
-        for s in sentences:
-            outfile.write(s + '\n')
-        # output <\index> to indicate the end of the current document
-        outfile.write('</' + str(doc_index) + '>\n')
-        outfile.write('\n')
-        doc_index += 1
-    print('The unprocessed data has been split into sentences by space. ')
-    print('The result is output in ' + SPLIT_CACHE)
-    outfile.close()
-    # test deleting stop word
-    infile = open(SPLIT_CACHE, 'r')
-    outfile = open(DEL_STOP_WORD_CACHE, 'w')
-    lines = infile.read()
-    lines = lines.split('\n')
-    # delete stop words
-    for l in lines:
-        sentence = delete_stop_words(l)
-        outfile.write(sentence + '\n')
-    print('The split sentences has been free of stop words. ')
-    print('The result is output in ' + DEL_STOP_WORD_CACHE)
-    outfile.close()
-    # test stemming
-    infile = open(DEL_STOP_WORD_CACHE, 'r')
-    outfile = open(STEM_CACHE, 'w')
-    lines = infile.read()
-    lines = lines.split('\n')
-    # stem sentences of all lines
-    sentences = porter_stemming(lines)
-    for s in sentences:
-        outfile.write(s + '\n')
-    print('The stop-word-free sentences has been stemmed. ')
-    print('The result is output in ' + STEM_CACHE)
-    outfile.close()
-
-    print(tf_idf())
+    summary = summarize(doc_path=DOC_PATH,
+                        doc_list=DOCUMENTS)
+    print(summary)
